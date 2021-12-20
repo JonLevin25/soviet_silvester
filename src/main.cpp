af56ace3e5d769c458b0812ab0ef24aef89d54e3
Jonathan Levin <jon.levin25@gmail.com>
2021-12-20 14:07:28 +0200

    consts for calculations

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,6 @@
 
 #define DAC_PIN 25
 
-// don't fully understand why delay needs to be _half_ the sample length
-#define SAMPLE_DELAY_MICROS ((uint32_t) round(SAMPLE_LENGTH_SECS * 1E6))
-#define SAMPLE_LENGTH_SECS (1.0/SAMPLE_FREQ)
-
 
 #define SAMPLE_FREQ 16000
 #define PUTIN putin_test_16khz_8bit_20sec_wav
@@ -19,6 +15,9 @@
 // #define PUTIN putin_test_32khz_8bit_20sec_wav
 // #define PUTIN_LEN putin_test_32khz_8bit_20sec_wav_len
 
+const double SAMPLE_LENGTH_SECS = 1.0 / SAMPLE_FREQ;
+const uint32_t SAMPLE_DELAY_MICROS = (uint32_t) round(SAMPLE_LENGTH_SECS * 1E6);
+
 void setup()
 {
   Serial.begin(9600);
@@ -26,7 +25,7 @@ void setup()
   for (size_t i = 0; i < PUTIN_LEN; i++)
   {
     uint8_t sample = PUTIN[i];
-    dacWrite(DAC_PIN,sample);
+    dacWrite(DAC_PIN, sample);
     delayMicroseconds(SAMPLE_DELAY_MICROS);
   }
   Serial.println("===== END SOUND TEST =====");
