e0829e48fa677fd335e38138218da6f253739386
Jonathan Levin <jon.levin25@gmail.com>
2021-12-26 17:46:50 +0200

    dac Tone test

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,55 @@
 #include <Arduino.h>
 
-#include "sound/putin_test_16khz_8bit_20sec.h"
-#include "sound/putin_test_32khz_8bit_20sec.h"
+const uint16_t freqs[] = {
+  50,
+  100,
+  150,
+  200,
+  400,
+  1000,
+  2000,
+};
+const size_t freqs_len = sizeof(freqs) / sizeof(freqs[0]);
 
 #define DAC_PIN 25
+#define TONE_LEN_MILLIS 1000U
+#define WAIT_LEN_MILLIS 250U
 
+void playToneDac(uint16_t freq, uint16_t millis, uint8_t amp)
+{ 
+  const uint32_t start_micros = micros();
+  const uint32_t end_micors = start_micros + millis * 1e3;
+  const uint32_t delay_micros = (uint32_t) round((0.5/freq)*1e6); // delay half so we can set peaks AND troughs
 
-#define SAMPLE_FREQ 16000
-#define WAV_DATA putin_test_16khz_8bit_20sec_wav
-#define WAV_DATA_LEN putin_test_16khz_8bit_20sec_wav_len
-
-
-// #define SAMPLE_FREQ 32000
-// #define WAV_DATA putin_test_32khz_8bit_20sec_wav
-// #define WAV_DATA_LEN putin_test_32khz_8bit_20sec_wav_len
-
-const double SAMPLE_LENGTH_SECS = 1.0 / SAMPLE_FREQ;
-const uint32_t SAMPLE_DELAY_MICROS = (uint32_t) round(SAMPLE_LENGTH_SECS * 1E6);
-
-int32_t err_sampleget = 0;
-int32_t err_dacWrite = 0;
-int32_t err_errs = 0;
-
-void updateMaxError(uint32_t actual, uint32_t expected, int32_t *ptrMaxError) { 
-  bool errPositive = actual > expected;
-  uint32_t uerror = errPositive
-    ? (actual - expected)
-    : (expected - actual);
-
-  if (uerror < *ptrMaxError) return;
-
-  *ptrMaxError = errPositive
-    ? +((int32_t)uerror)
-    : -((int32_t)uerror);
-}
-
-void play()
-{
-  Serial.printf("Delay micros: %u\n", SAMPLE_DELAY_MICROS);
-  Serial.println("===== BEGIN SOUND TEST =====");
-  
-  uint32_t start_micros = micros();
-  for (size_t i = 0; i < WAV_DATA_LEN; i++)
+  uint32_t cur_micros = start_micros;
+  uint32_t i = 0;
+  do
   {
-    uint8_t sample = WAV_DATA[i];
-    dacWrite(DAC_PIN, sample);
+    // Todo simplify for article
+    uint8_t val = (i++ % 2) == 0
+      ? 0
+      : amp;
+
+    dacWrite(DAC_PIN, val);
     
     // instead of delaying a constant amount,
     // calculate it relative to how far behind we are
-    uint32_t next_sample_time = start_micros + SAMPLE_DELAY_MICROS * (i+1);
+    uint32_t next_sample_time = start_micros + delay_micros * (i+1);
     delayMicroseconds(next_sample_time - micros());
-  }
-
-  Serial.println("===== END SOUND TEST =====");
+    cur_micros = micros();
+  } while(cur_micros < end_micors);
 }
-  // Serial.printf("Errors: ArrayAccess: %u, DacWrite: %u, Errors: %u\n", err_sampleget, err_dacWrite, err_errs);
 
 void setup()
 {
   Serial.begin(9600);
-  delay(1500);
-  play();
+  Serial.println(freqs_len);
+  for (size_t i = 0; i < freqs_len; i++)
+  {
+    Serial.printf("Playing freq: %u\n", freqs[i]);
+    playToneDac(freqs[i], TONE_LEN_MILLIS, 128);
+    delay(WAIT_LEN_MILLIS);
+  }
 }
 
-void loop(){ }
+void loop(){}
